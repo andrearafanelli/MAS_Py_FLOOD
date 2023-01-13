6a3ea62b47d39529d3b67363d9400c97eb34eb5f
andrearafanelli <andrea.rafanelli96@gmail.com>
2023-01-13 13:25:01 +0100

    update MAS

---
+++ MAS/DALI/mas/adjacent.pl
@@ -0,0 +1,17 @@
+:- dynamic adjacent/2.
+adjacent(0, 5).
+adjacent(1, 5).
+adjacent(1, 6).
+adjacent(2, 5).
+adjacent(3, 5).
+adjacent(3, 7).
+adjacent(3, 8).
+adjacent(4, 5).
+adjacent(5, 0).
+adjacent(5, 1).
+adjacent(5, 2).
+adjacent(5, 3).
+adjacent(5, 4).
+adjacent(6, 1).
+adjacent(7, 3).
+adjacent(8, 3).
